0ff5bd0f91bd70d92b87f3dc4a36c7d52771eb52
eshengsky <eshengsky@163.com>
2019-04-19 18:04:43 +0800

    commit

--- popup.js
+++ popup.js
@@ -5,14 +5,24 @@ document.body.addEventListener('click', evt => {
     }
 });
 
+// listen icon load fail
+document.addEventListener('error', err => {
+    const el = err.target;
+    if (el.nodeName === 'IMG') {
+        el.style.display = 'none';
+    }
+}, true)
+
 const listEl = document.querySelector('#sitesList');
 chrome.topSites.get(topSites => {
     listEl.innerHTML = '';
     if (topSites && Array.isArray(topSites) && topSites.length > 0) {
         let html = '';
         topSites.forEach(site => {
+            const url = new URL(site.url);
+            const iconImg = `${url.protocol}//${url.host}/favicon.ico`;
             html += `<li>
-                <a href="${site.url}" title="${site.title}">${site.title}</a>
+                <a href="${url.href}" title="${site.title}"><span><img src="${iconImg}"/></span>${site.title}</a>
             </li>`;
         });
         listEl.innerHTML = html;
